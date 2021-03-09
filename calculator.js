8a59bd4be27991c7501300c1555c2247fcc84c3d
Ponce <poncemathieu@gmail.com>
2021-03-09 22:34:50 +0100

    step3

--- calculator.js
+++ calculator.js
@@ -4,6 +4,4 @@ const operator = prompt("What's the operator ?");
 
 const secondValue = prompt("What's the second value ?");
 
-console.log('firstValue');
-console.log('operator');
-console.log('secondValue');
+console.log(parseInt(firstValue) + parseInt(secondValue));
