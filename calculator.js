6a462b70eff16ae581aa98b75ebdfdf53049e492
Ponce <poncemathieu@gmail.com>
2021-03-09 22:19:03 +0100

    step2

--- calculator.js
+++ calculator.js
@@ -1,1 +1,9 @@
-console.log('Hello World !');
+const firstValue = prompt("What's the value ?");
+
+const operator = prompt("What's the operator ?");
+
+const secondValue = prompt("What's the second value ?");
+
+console.log('firstValue');
+console.log('operator');
+console.log('secondValue');
